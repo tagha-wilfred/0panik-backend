312becbf944f45cb6e0ea0b2b578e1bfe9b19440
Wilfred Tagha <wilfred@mrdayter.com>
2026-07-15 05:46:32 +0100

    better import

--- devices/admin.py
+++ devices/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from ..accounts.models import User 
+from accounts.models import User 
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'full_name', 'is_staff', 'date_joined')
